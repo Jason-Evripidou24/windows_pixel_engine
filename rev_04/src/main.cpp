b8f4971da63640d8c9b296cb7594c18a1b3e17d6
Jason-Evripidou <69157142+Jason-Evripidou@users.noreply.github.com>
2026-07-09 16:37:37 +1000

    Update

--- rev_04/src/main.cpp
+++ rev_04/src/main.cpp
@@ -40,7 +40,12 @@ static Camera camera
     Math::Vec3_f(0.0f, 1.0f, 0.0f)
 );
 
-static float camera_move_speed = 0.005f;
+static bool tracking_mouse = false;
+static float mouse_pos_x = 0.0f;
+static float mouse_pos_y = 0.0f;
+
+static float camera_move_speed = 0.5f;
+static float camera_look_speed = 0.01f;
 void processInput(Window& window)
 {
     // Move camera along x
@@ -52,6 +57,42 @@ void processInput(Window& window)
     // Move camera along z
     if(window.m_input.isKeyDown('W') == true) { camera.moveForward(camera_move_speed); }
     if(window.m_input.isKeyDown('S') == true) { camera.moveForward(-camera_move_speed); }
+
+    if(tracking_mouse == true)
+    {
+        // Stop tracking mouse if left mouse button is pressed.
+        if(window.m_input.isMousePressed(0) == true)
+        {
+            tracking_mouse = false;
+            mouse_pos_x = 0.0f;
+            mouse_pos_y = 0.0f;
+        }
+        else
+        {
+            // Pitch camera up and down.
+            float curr_mouse_pos_x = window.m_input.m_mouse_x;
+            float curr_mouse_pos_y = window.m_input.m_mouse_y;
+
+            float mouse_dx = mouse_pos_x - curr_mouse_pos_x;
+            float mouse_dy = mouse_pos_y - curr_mouse_pos_y;
+
+            camera.lookRight(mouse_dx * camera_look_speed);
+            camera.lookUp(mouse_dy * camera_look_speed);
+
+            mouse_pos_x = curr_mouse_pos_x;
+            mouse_pos_y = curr_mouse_pos_y;
+        }
+    }
+    else
+    {
+        // Start tracking mouse if left mouse button is pressed.
+        if(window.m_input.isMousePressed(0) == true)
+        {
+            tracking_mouse = true;
+            mouse_pos_x = window.m_input.m_mouse_x;
+            mouse_pos_y = window.m_input.m_mouse_y;
+        }
+    }
 }
 // ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### //
 
@@ -60,13 +101,21 @@ void processInput(Window& window)
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
 {
     //---------------------------------------------------------------------------------------------------------------------//
+    Mesh road_mesh = Mesh::roadMesh(0xFF000000);
+    Model road_model = Model();
+    road_model.m_mesh = &road_mesh;
+    road_model.m_position = Math::Vec3_f(0.0f, 0.0f, 0.0f);
+    road_model.m_scale = Math::Vec3_f(1.0f, 1.0f, 1.0f);
+    road_model.m_rotate_rad = 0.0f;
+    road_model.m_rotate_axis = Math::Vec3_f(0.0f, 1.0f, 0.0f);
+
     static std::vector<Mesh> cube_meshes(3);
     // Center cube.
-    cube_meshes[0] = Mesh::createCubeMesh(0xFFFFFF00);
+    cube_meshes[0] = Mesh::cubeMesh(0xFFFFFF00);
     // Positive color.
-    cube_meshes[1] = Mesh::createCubeMesh(0xFF000000);
+    cube_meshes[1] = Mesh::cubeMesh(0xFF000000);
     // Negative color.
-    cube_meshes[2] = Mesh::createCubeMesh(0xFFFFFFFF);
+    cube_meshes[2] = Mesh::cubeMesh(0xFFFFFFFF);
 
     static std::vector<Model> cube_models(7);
     // Center cube.
@@ -133,7 +182,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
         backbuffer.clear(0xFF87CEFA);
 
         processInput(window);
-        
+
         Math::Mat4_f cam_projection = camera.calcProjectionMatrix((float)backbuffer.m_width / (float)backbuffer.m_height);
         Math::Mat4_f cam_view = camera.calcViewMatrix();
         Math::Mat4_f cam_proj_cam_view = cam_projection * cam_view;
@@ -145,11 +194,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
             cube_models[i].m_rotate_rad = angle;
             Math::Mat4_f model = cube_models[i].calcModelMatrix();
             renderer.drawModel(backbuffer, cube_models[i], cam_proj_cam_view);
+
+            for(int i = 0; i < 100; i++)
+            {
+                road_model.m_position.m_data[2] = (float)i;
+                renderer.drawModel(backbuffer, road_model, cam_proj_cam_view);
+            }
         }
-
-        std::string info_string = camera.toString(6, 2);
-        backbuffer.setText(10, 10, info_string.c_str(), info_string.size(), 0xFF000000);
-
 
         backbuffer.present(window.m_dc, window.m_width, window.m_height);
     }
---
+++ rev_04/src/model/implementation_mesh/roadMesh.cpp
@@ -0,0 +1,87 @@
+// ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### //
+//-------------------------------------------------------------------------------------------------------------------------//
+// Standard library.
+//-------------------------------------------------------------------------------------------------------------------------//
+#include <vector>
+//-------------------------------------------------------------------------------------------------------------------------//
+
+//-------------------------------------------------------------------------------------------------------------------------//
+// Third party.
+//-------------------------------------------------------------------------------------------------------------------------//
+//-------------------------------------------------------------------------------------------------------------------------//
+
+//-------------------------------------------------------------------------------------------------------------------------//
+// Internal.
+//-------------------------------------------------------------------------------------------------------------------------//
+#include "../mesh.hpp"
+//-------------------------------------------------------------------------------------------------------------------------//
+// ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### //
+
+
+// ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### //
+Mesh Mesh::roadMesh(uint32_t color)
+{
+    Mesh road_mesh;
+    
+    int cross_section_vertex_count = 13;
+    int num_cross_sections = 2;
+
+    int triangles_count = ((cross_section_vertex_count - 1) * 2) * (num_cross_sections - 1);
+
+    //---------------------------------------------------------------------------------------------------------------------//
+    // Vertices: Position and colour.
+    //---------------------------------------------------------------------------------------------------------------------//
+    float cen_x  = 0.0f;    float cen_y  = 0.45f;
+    float leb_x  = -4.0f;   float leb_y  = 0.15f;           float reb_x  = 4.0f;   float reb_y  = 0.15f;
+    float linv_x = -4.3f;   float linv_y = 0.0f;            float rinv_x = 4.3f;   float rinv_y = 0.0f;
+    float ltk_x  = -4.4f;   float ltk_y  = 0.45f;           float rtk_x  = 4.4f;   float rtk_y  = 0.45f;
+    float lbk_x  = -4.5f;   float lbk_y  = 0.45f;           float rbk_x  = 4.5f;   float rbk_y  = 0.45f;
+    float lfp_x  = -5.0f;   float lfp_y  = 0.5f;            float rfp_x  = 5.0f;   float rfp_y  = 0.5f;
+    float lbp_x  = -6.0f;   float lbp_y  = 0.5f;            float rbp_x  = 6.0f;   float rbp_y  = 0.5f;
+
+    road_mesh.m_vertices = std::vector<Vertex>(cross_section_vertex_count * num_cross_sections);
+
+    for(int i = 0; i < num_cross_sections; i++)
+    {
+        float z_pos = (float)i;
+
+        // LHS
+        road_mesh.m_vertices[(i * cross_section_vertex_count) + 0].m_position = Math::Vec4_f(lbp_x, lbp_y, z_pos, 1.0f);
+        road_mesh.m_vertices[(i * cross_section_vertex_count) + 1].m_position = Math::Vec4_f(lfp_x, lfp_y, z_pos, 1.0f);
+        road_mesh.m_vertices[(i * cross_section_vertex_count) + 2].m_position = Math::Vec4_f(lbk_x, lbk_y, z_pos, 1.0f);
+        road_mesh.m_vertices[(i * cross_section_vertex_count) + 3].m_position = Math::Vec4_f(ltk_x, ltk_y, z_pos, 1.0f);
+        road_mesh.m_vertices[(i * cross_section_vertex_count) + 4].m_position = Math::Vec4_f(linv_x, linv_y, z_pos, 1.0f);
+        road_mesh.m_vertices[(i * cross_section_vertex_count) + 5].m_position = Math::Vec4_f(leb_x, leb_y, z_pos, 1.0f);
+
+        // Center
+        road_mesh.m_vertices[(i * cross_section_vertex_count) + 6].m_position = Math::Vec4_f(cen_x, cen_y, z_pos, 1.0f);
+
+        // RHS
+        road_mesh.m_vertices[(i * cross_section_vertex_count) + 7].m_position = Math::Vec4_f(reb_x, reb_y, z_pos, 1.0f);
+        road_mesh.m_vertices[(i * cross_section_vertex_count) + 8].m_position = Math::Vec4_f(rinv_x, rinv_y, z_pos, 1.0f);
+        road_mesh.m_vertices[(i * cross_section_vertex_count) + 9].m_position = Math::Vec4_f(rtk_x, rtk_y, z_pos, 1.0f);
+        road_mesh.m_vertices[(i * cross_section_vertex_count) + 10].m_position = Math::Vec4_f(rbk_x, rbk_y, z_pos, 1.0f);
+        road_mesh.m_vertices[(i * cross_section_vertex_count) + 11].m_position = Math::Vec4_f(rfp_x, rfp_y, z_pos, 1.0f);
+        road_mesh.m_vertices[(i * cross_section_vertex_count) + 12].m_position = Math::Vec4_f(rbp_x, rbp_y, z_pos, 1.0f);
+    }
+    for(int i = 0; i < cross_section_vertex_count * num_cross_sections; i++)
+    {
+        road_mesh.m_vertices[i].m_color = color;
+    }
+    //---------------------------------------------------------------------------------------------------------------------//
+
+    //---------------------------------------------------------------------------------------------------------------------//
+    // Indicies: Each triplet represents a triangle face.
+    //---------------------------------------------------------------------------------------------------------------------//
+    road_mesh.m_triangles = std::vector<Math::Vec3_i>(triangles_count);
+
+    for(int i = 0; i < cross_section_vertex_count - 1; i++)
+    {
+        road_mesh.m_triangles[(i * 2) + 0] = Math::Vec3_i(i, i + cross_section_vertex_count, i + cross_section_vertex_count + 1);
+        road_mesh.m_triangles[(i * 2) + 1] = Math::Vec3_i(i, i + cross_section_vertex_count, i + 1);
+    }
+    //---------------------------------------------------------------------------------------------------------------------//
+
+    return road_mesh;
+}
+// ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### //
